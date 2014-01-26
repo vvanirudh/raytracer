28f96aa428e169831b4e22839b179051f79389a7
Anirudh <vvanirudh@cse.iitb.ac.in>
2014-01-26 19:27:38 +0530

    Added Object_t

--- Object_t.h
+++ Object_t.h
@@ -3,10 +3,16 @@
 
 #include "Vector_t.h"
 #include "Point_t.h"
+#include "color_t.h"
 
 class Object_t
 {
+public:
+	Vector_t normal;
 
+	Color_t diffuseColor, specularColor;
+
+	double reflectance, transmittance;
 };
 
 #endif
